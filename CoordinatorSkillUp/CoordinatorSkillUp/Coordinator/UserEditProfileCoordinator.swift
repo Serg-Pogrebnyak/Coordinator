a62be5e059d45bad1d8b3d2b339d7e451dc3b0ca
Sergey Pogrebnyak <pogrebnyak.vetal1996@gmail.com>
2019-04-05 10:14:25 +0300

    fixed struct of project

---
+++ CoordinatorSkillUp/CoordinatorSkillUp/Coordinator/UserEditProfileCoordinator.swift
@@ -0,0 +1,13 @@
+//
+//  UserEditProfileCoordinator.swift
+//  CoordinatorSkillUp
+//
+//  Created by Sergey Pohrebnuak on 4/5/19.
+//  Copyright © 2019 Sergey Pohrebnuak. All rights reserved.
+//
+
+import Foundation
+
+protocol UpdateWithUser: class {
+    var user: User? {get set}
+}
